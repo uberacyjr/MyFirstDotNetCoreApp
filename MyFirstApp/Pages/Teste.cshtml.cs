8fe643d556aac6d7e29ea8fe17805233b1d07ace
Uberacy Tottoli <bera@boldtech.com.br>
2018-11-24 19:08:35 -0200

    Add razor page teste

---
+++ MyFirstApp/Pages/Teste.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstApp.Web.Pages
+{
+    public class TesteModel : PageModel
+    {
+        public string Descricao { get; set; }
+        public string Titulo { get; set; }
+        public void OnGet()
+        {
+            Titulo = "Razor Pages";
+            Descricao = "This tutorial teaches the basics of building an ASP.NET Core Razor Pages web app.";
+        }
+    }
+}
